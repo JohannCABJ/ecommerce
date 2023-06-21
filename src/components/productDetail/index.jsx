2a5a2a68cc0b5c1a77895bb701d6b62a0111f262
Johann Triana <johann.rvs@gmail.com>
2023-06-20 20:43:05 -0500

    change icon close

--- src/components/productDetail/index.jsx
+++ src/components/productDetail/index.jsx
@@ -1,5 +1,7 @@
 
 import { useContext } from 'react'
+import {ChevronDoubleDownIcon} from '@heroicons/react/24/solid'
+
 import { ShoppingCartContext } from '../../context'
 import './styles.css'
 
@@ -12,11 +14,7 @@ const ProductDetail = () => {
       <div className='flex justify-between items-center p-6'>
         <h2 className='font-medium text-xl'>Detail</h2>
         <div>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              onClick={() => context.closeProductDetail()}
-            />
-          </svg>
+        <ChevronDoubleDownIcon className="h-6 w-6 text-blue-500"  onClick={() => context.closeProductDetail()}/>
         </div>
       </div>
       <figure className='px-6'>
